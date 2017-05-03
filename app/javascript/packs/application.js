fa3e994456fdee7773e11a3d3bb7770c137199d5
devico <clamdm@mail.ru>
2017-05-03 14:07:21 +0300

    refactoring js code for flickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,40 +1,26 @@
 // app/javascript/packs/application.js
+'use strict'
 
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 
+//
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  Vue.use(VueResource)
+  
   new Vue({
-    el: '#vue-cards-form',
-    data: function(){
-      return {
-        form_obj: {
-          original_text: '',
-          translated_text: '',
-          image: '',
-          block: ''
-        }
-      }
+    el: '#flickr-block',    
+    data: {
+      tag: ''
     },
     methods: {
-      onSubmit: function(event) {
-        var self = this;
-        this.$http.post('/cards', { cards: self.form_obj })
+      searchFlickr: function() {
+        console.log('help me!')
+        // let self = this;
+        // this.$http.get('/flickrs', { flickrs: self.tag })
       }
-    }
+    }   
   });
-
-  new Vue({
-    el: "#flickr-block",
-    data: function(){
-      return {
-        tags: ''
-        }
-      }
-    },
-    methods: {
-      searchFlickr: function(event) {
-        var self = this;
-        this.$http.get('/flickrs', { flickrs: self.tags })
-      }
-    }
-          
-  });
+});

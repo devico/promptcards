13b83748187c8cde1aa48bf7a9ee5e68561a6fcf
Ivan Shamatov <status.enable@gmail.com>
2017-05-03 18:04:45 +0300

    flickr component

---
+++ app/javascript/components/flickr.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="tag">  
+    <button type="submit" v-on:click.prevent="searchFlickr">Найти во Flickr</button>         
+  </div>  
+</template>
+
+<script>
+  const el = '#flickr'
+
+  function data() {
+    return {
+      tag: ''
+    }
+  }
+
+  function searchFlickr() { 
+    console.log('help me')
+  }
+
+  export default {
+    el, data,
+    methods: {
+      searchFlickr
+    }
+  }
+</script>
